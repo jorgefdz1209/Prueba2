90f58544dfc4d504b649a866957dce5145b04735
jorgefdz <jorgefdz1209@gmail.com>
2018-09-04 11:01:32 -0500

    Initial Commit

---
+++ 15-08-2018/src/HolaMundo.java
@@ -0,0 +1,14 @@
+/**
+ * Clase holamundo para la impresion del hola mundo en java  
+ * @author jorge
+ *
+ */
+public class HolaMundo {
+	/**
+	 * Metodo main para la impresion del hola mundo
+	 * @param args String[] Arreglo de String que sirve como argumentos para la ejecucion de una aplicacion en JAVA
+	 */
+	public static void main(String args[]){
+		System.out.println("Hola Mundo");
+	}
+}
